b288bae68b8fb50b5764a3509f72eb8109a35951
Prasanna <prasanna.narayanan2021@gmail.com>
2019-10-12 20:54:38 +0530

    update initial state

--- src/state/initialState.js
+++ src/state/initialState.js
@@ -1,13 +1,13 @@
 import profileImages from "../assets/profile-pics/loadProfilePics";
 
 export default {
-    name: 'Karthikeyini SK',
+    name: 'Prasanna L',
     logo: profileImages[0],
     location: 'Chennai, India',
     accountInformation: {
-        email: 'karthikeyini@gmail.com',
+        email: 'prasanna.narayanan2021@gmail.com',
         password: '*********',
-        dob: "November 14, 1997",
+        dob: "November 24, 1997",
         bio: "Biodata is an abbreviation for the term biographical data. ... The biodata format includes relevant factual information about an individual, such as: personal information (e.g., date of birth, gender, marital status, religion, height, complexion, father's name, etc.) educational background. occupational history."
     },
     settings: [
@@ -18,7 +18,7 @@ export default {
     ],
     contacts: {
         phone: "+91 9983092199",
-        email: "karthikeyini@gmail.com",
+        email: "prasanna.narayanan2021@gmail.com",
         link: "www.google.com"
     },
     activities: [
