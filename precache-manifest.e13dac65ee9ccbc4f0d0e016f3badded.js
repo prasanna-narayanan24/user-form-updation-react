282b74b56289951da96c03ec78e259737a2fa947
Prasanna <prasanna.narayanan2021@gmail.com>
2019-10-12 19:54:24 +0530

    Updates

---
+++ precache-manifest.e13dac65ee9ccbc4f0d0e016f3badded.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b1db8fcb785985d930781c77d9748ec",
+    "url": "/user-form-updation-react/index.html"
+  },
+  {
+    "revision": "275b95fe714205489c12",
+    "url": "/user-form-updation-react/static/css/2.4e0883bb.chunk.css"
+  },
+  {
+    "revision": "75226350a6453e19e8a7",
+    "url": "/user-form-updation-react/static/css/main.51a12ef0.chunk.css"
+  },
+  {
+    "revision": "275b95fe714205489c12",
+    "url": "/user-form-updation-react/static/js/2.cfee7544.chunk.js"
+  },
+  {
+    "revision": "75226350a6453e19e8a7",
+    "url": "/user-form-updation-react/static/js/main.99e82a21.chunk.js"
+  },
+  {
+    "revision": "54e9e37e0f76315abf94",
+    "url": "/user-form-updation-react/static/js/runtime-main.7178f56f.js"
+  },
+  {
+    "revision": "44a5c053241c898dc64dac9f2f8eb23d",
+    "url": "/user-form-updation-react/static/media/boy-1.44a5c053.png"
+  },
+  {
+    "revision": "99e8fdf67b4c0bee564e6bd6ee1718dd",
+    "url": "/user-form-updation-react/static/media/boy-2.99e8fdf6.png"
+  },
+  {
+    "revision": "d7fd9e0b952d5f9b9adff6ec29a8b20d",
+    "url": "/user-form-updation-react/static/media/boy.d7fd9e0b.png"
+  },
+  {
+    "revision": "0a7ca2a281c6fbd59b613f0672ab0cd2",
+    "url": "/user-form-updation-react/static/media/girl-1.0a7ca2a2.png"
+  },
+  {
+    "revision": "e1b10c9b5fa4278bdfa72f44e5076c3f",
+    "url": "/user-form-updation-react/static/media/girl-2.e1b10c9b.png"
+  },
+  {
+    "revision": "543cca41f0f6fd0b4ec44abccbfefb02",
+    "url": "/user-form-updation-react/static/media/girl.543cca41.png"
+  },
+  {
+    "revision": "6f2bab193b29900a280514754925b38e",
+    "url": "/user-form-updation-react/static/media/man.6f2bab19.png"
+  }
+]);
